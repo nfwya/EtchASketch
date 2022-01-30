52d328c63bab8539659face8ab5ea5bba25e05c3
Doc_husky <nfwya0811@gmail.com>
2022-01-30 20:51:25 +0800

    finished etch and sketch

--- draw.js
+++ draw.js
@@ -10,6 +10,7 @@ const drawBoard = function(gridNum){
         body.removeChild(oldBoard);
     }
     body.appendChild(board); 
+    initiateBoard();
 }
 
 const drawRow = function(gridNum){
@@ -22,12 +23,29 @@ const drawRow = function(gridNum){
     }
     return newRow;
 }
-drawBoard(5);
+drawBoard(4);
 
-/* Event Listener */
-const cells = document.querySelectorAll(".cell")
-const etch = function(e){
-    this.classList.add("hovered");
-    //setTimeout(function(){this.classList.remove('hovered')},500);
+/* Event Listener of cells*/
+function initiateBoard(){
+    const cells = document.querySelectorAll(".cell")
+    const etch = function(e){
+        this.classList.add("hovered");
+        this.removeEventListener('mouseover',etch);
+        //setTimeout(function(){this.classList.remove('hovered')},500);
+    }
+    cells.forEach( cell => cell.addEventListener('mouseover', etch ) );
 }
-cells.forEach( cell => cell.addEventListener('mouseover', etch ) );
+/* Event Listener of button */
+const resetButton = document.querySelector(".reset");
+const reset = function(e){
+    const gridNum = Number( prompt("Please enter a number for grid:") );
+    if( ( typeof(gridNum) !== 'number') ||
+    gridNum>100 || gridNum <0 ){
+        alert("ERROR! Used default grid number");
+        drawBoard(4);
+    }
+    else{
+        drawBoard(gridNum);
+    }
+}
+resetButton.addEventListener('click',reset);
